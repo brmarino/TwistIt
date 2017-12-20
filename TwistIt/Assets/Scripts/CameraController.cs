e9bc2481f6415c9762c079f5132c2464d4ad39bd
brmarino <brandonrmarino@gmail.com>
2017-12-19 22:06:37 -0800

    My first commit

---
+++ TwistIt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        //player = GameObject.FindGameObjectsWithTag("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z - 3.5f);
+    }
+}
---
+++ TwistIt/Assets/Scripts/PlayOn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOn : MonoBehaviour {
+
+    private bool right, left = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            left = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            right = true;
+        }
+
+        if (right)
+        {
+            //transform.Rotate(0, 0, -45);
+            //right = false;
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, transform.localEulerAngles + Vector3.forward * -45, Time.deltaTime * 5);
+            Invoke("ResetAll", 0.2f);
+        }
+        else if (left)
+        {
+            //transform.Rotate(0, 0, 45);
+            //left = false;
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, transform.localEulerAngles + Vector3.forward * 45, Time.deltaTime * 5);
+            Invoke("ResetAll", 0.2f);
+        }
+    }
+
+    void ResetAll()
+    {
+        if (Mathf.Abs(transform.localEulerAngles.z % 45) > 0 && right)
+        {
+            transform.localEulerAngles -= Vector3.forward * (transform.localEulerAngles.z % 45 - 45);
+        }
+        else if (Mathf.Abs(transform.localEulerAngles.z % 45) > 0 && left)
+        {
+            transform.localEulerAngles -= Vector3.forward * (transform.localEulerAngles.z % 45);
+        }
+        right = false;
+        left = false;
+    }
+}
